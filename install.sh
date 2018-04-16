4b57725a4ed6d316898f71598a48289640b72804
Genroh <lightningrod.marowak.105@gmail.com>
2018-04-16 17:57:04 +0000

    git clone に sudo をつけるな

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 # for Ubuntu
 sudo apt-get install git gcc make openssl libssl-dev libbz2-dev libreadline-dev libsqlite3-dev
-sudo git clone https://github.com/pyenv/pyenv.git ./pyenv
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 
 cp .profile ~/
 cp .bashrc ~/
